e8fdc2af0d3cbf3c5d138867e1ef755e9e3487c2
deepmouse <deepmouse@mouseland.org>
2021-05-01 18:32:36 +0300

    Clean up unused plugins and configs

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,11 +1,4 @@
 const preprocess = require('svelte-preprocess');
-//const { nodeResolve } = require('@rollup/plugin-node-resolve');
-//const commonjs = require('@rollup/plugin-commonjs')
-//const nodePolyfills = require('rollup-plugin-node-polyfills')
-//const globals = require('rollup-plugin-node-globals')
-//const builtins = require('rollup-plugin-node-builtins')
-//const polyfillNode = require('rollup-plugin-polyfill-node');
-//const builtinsPlugin = { ...builtins({ crypto: true }), name: 'rollup-plugin-node-builtins' }
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -15,18 +8,6 @@ module.exports = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			define: {
-				'process.env': {}
-			},
-			rollupOptions: {
-				plugins: [
-					//nodeResolve({ browser: true }),
-					//commonjs(),
-					//nodePolyfills()
-				]
-			}
-		}
+		target: '#svelte'
 	}
 };
